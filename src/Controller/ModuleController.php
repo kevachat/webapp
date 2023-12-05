fb9de3cf1b585b19559f25e3a150cb1ac85695ff
ghost <localhost>
2023-12-05 04:15:41 +0200

    implement basic features

---
+++ src/Controller/ModuleController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class ModuleController extends AbstractController
+{
+    public function info(): Response
+    {
+        $client = new \Kevachat\Kevacoin\Client(
+            $this->getParameter('app.kevacoin.protocol'),
+            $this->getParameter('app.kevacoin.host'),
+            $this->getParameter('app.kevacoin.port'),
+            $this->getParameter('app.kevacoin.username'),
+            $this->getParameter('app.kevacoin.password')
+        );
+
+        return $this->render(
+            'default/module/info.html.twig',
+            [
+                'wallet' =>
+                [
+                    'balance' => (float) $client->getBalance(),
+                    'block'   => (int) $client->getBlockCount(),
+                    'address' => $this->getParameter('app.kevacoin.address')
+                ],
+                'pool' => [
+                    'url' => $this->getParameter('app.kevacoin.pool.url')
+                ]
+            ]
+        );
+    }
+
+    public function room(
+        Request $request
+    ): Response
+    {
+        $client = new \Kevachat\Kevacoin\Client(
+            $this->getParameter('app.kevacoin.protocol'),
+            $this->getParameter('app.kevacoin.host'),
+            $this->getParameter('app.kevacoin.port'),
+            $this->getParameter('app.kevacoin.username'),
+            $this->getParameter('app.kevacoin.password')
+        );
+
+        return $this->render(
+            'default/module/room.html.twig',
+            [
+                'room' => $request->get('room'),
+                'list' => (array) $client->kevaListNamespaces()
+            ]
+        );
+    }
+
+    public function post(
+        Request $request
+    ): Response
+    {
+        $client = new \Kevachat\Kevacoin\Client(
+            $this->getParameter('app.kevacoin.protocol'),
+            $this->getParameter('app.kevacoin.host'),
+            $this->getParameter('app.kevacoin.port'),
+            $this->getParameter('app.kevacoin.username'),
+            $this->getParameter('app.kevacoin.password')
+        );
+
+        // Get wallet namespaces (to enable post module there)
+        $namespaces = [];
+
+        foreach ((array) $client->kevaListNamespaces() as $key => $value)
+        {
+            $namespaces[] = $key;
+        }
+
+        return $this->render(
+            'default/module/post.html.twig',
+            [
+                'enabled'   => in_array($request->get('namespace'), $namespaces),
+                'namespace' => $request->get('namespace'),
+                'message'   => $request->get('message'),
+                'user'      => $request->get('user'),
+                'ip'        => $request->getClientIp()
+            ]
+        );
+    }
+}
---
+++ src/Controller/RoomController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class RoomController extends AbstractController
+{
+    #[Route(
+        '/',
+        name: 'room_index',
+        methods:
+        [
+            'GET'
+        ]
+    )]
+    public function index(): Response
+    {
+        return $this->redirectToRoute(
+            'room_namespace',
+            [
+                'namespace' => $this->getParameter('app.kevacoin.namespace')
+            ]
+        );
+    }
+
+    #[Route(
+        '/room/{namespace}',
+        name: 'room_namespace',
+        requirements:
+        [
+            'namespace' => '^[A-z0-9]{34}$',
+        ],
+        methods:
+        [
+            'GET'
+        ]
+    )]
+    public function room(
+        Request $request
+    ): Response
+    {
+        // Connect kevacoin
+        $client = new \Kevachat\Kevacoin\Client(
+            $this->getParameter('app.kevacoin.protocol'),
+            $this->getParameter('app.kevacoin.host'),
+            $this->getParameter('app.kevacoin.port'),
+            $this->getParameter('app.kevacoin.username'),
+            $this->getParameter('app.kevacoin.password')
+        );
+
+        // Get room messages
+        $messages = [];
+
+        foreach ((array) $client->kevaFilter($request->get('namespace')) as $message)
+        {
+            $messages[] = $message;
+        }
+
+        // Return result
+        return $this->render(
+            'default/room/index.html.twig',
+            [
+                'messages' => $messages,
+                'request'  => $request
+            ]
+        );
+    }
+
+    #[Route(
+        '/room/{namespace}/post',
+        name: 'room_post',
+        requirements:
+        [
+            'namespace' => '^[A-z0-9]{34}$',
+        ],
+        methods:
+        [
+            'POST'
+        ]
+    )]
+    public function post(
+        Request $request
+    ): Response
+    {
+        // Connect kevacoin
+        $client = new \Kevachat\Kevacoin\Client(
+            $this->getParameter('app.kevacoin.protocol'),
+            $this->getParameter('app.kevacoin.host'),
+            $this->getParameter('app.kevacoin.port'),
+            $this->getParameter('app.kevacoin.username'),
+            $this->getParameter('app.kevacoin.password')
+        );
+
+        // Check namespace exist for this wallet
+        $namespaces = [];
+
+        foreach ((array) $client->kevaListNamespaces() as $key => $value)
+        {
+            $namespaces[] = $key;
+        }
+
+        if (!in_array($request->get('namespace'), $namespaces))
+        {
+            exit('Namespace not related with this node!');
+        }
+
+        // @TODO
+
+        // Redirect back to the room
+        return $this->redirectToRoute(
+            'room_namespace',
+            [
+                'namespace' => $this->getParameter('app.kevacoin.namespace')
+            ]
+        );
+    }
+}
